1856a031f879f9cec260682d9045a75479aaf1f9
SebastianAle <xxsebastianxxale@yahoo.com>
2018-04-22 06:03:42 -0400

    Assignment4 completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import * as firebase from 'firebase';
 import RoomList from './components/RoomList';
+import MessageList from './components/MessageList';
 
 var config = {
     apiKey: "AIzaSyDkoSGjI5Ry-RsHtuoBy_H0GMZ_h_SY4-Q",
@@ -11,17 +12,38 @@ var config = {
     storageBucket: "bloc-chat-69d23.appspot.com",
     messagingSenderId: "766262905059"
   };
+
   firebase.initializeApp(config);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeRoom: "",
+      activeRoomKey: "",
+    };
+  }
+
+  handleActiveRoom(room) {
+    this.setState({ activeRoom: room });
+    this.setState({ activeRoomKey: room.key });
+  }
+
   render() {
     return (
       <div>
         <header>
           <h1>Bloc Chat</h1>
         </header>
+        <aside>
+          <RoomList firebase={firebase} activeRoom={this.handleActiveRoom.bind(this)}/>
+        </aside>
+        <div>
+          <h2>{this.state.activeRoom.name}</h2>
+        </div>
         <main>
-          <RoomList firebase={firebase} />
+          <MessageList firebase={firebase} activeRoomKey={this.state.activeRoomKey}/>
         </main>
       </div>
     );
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,62 +1,63 @@
 import React, { Component } from 'react';
 
-class RoomList extends Component{
+class RoomList extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       rooms: [],
-      newRoomName: ""
+      newRoomName: "",
     };
+
     this.createRoom = this.createRoom.bind(this);
     this.roomsRef = this.props.firebase.database().ref('rooms');
   }
 
-	componentDidMount() {
-	  this.roomsRef.on('child_added', snapshot => {
-	    const room = snapshot.val();
-	    room.key = snapshot.key;
-	    this.setState({ rooms: this.state.rooms.concat( room ) })
-	  	});
-	}
+  componentDidMount() {
+	this.roomsRef.on('child_added', snapshot => {
+	  const room = snapshot.val();
+	  room.key = snapshot.key;
+	  this.setState({ rooms: this.state.rooms.concat(room) })
+	});
+  }
 
-	createRoom(e){
-		e.preventDefault();
-		const inputText = document.getElementById("crname").value;
-		if(inputText.length < 5){
-			alert("Room name must be at least 5 characters");
-		}
-		else{
-			const newRoomName = this.state.newRoomName;
-		this.roomsRef.push({ 
-			name: newRoomName
-		});
-		this.setState({ newRoomName: "" });
-		}
+  createRoom(e) {
+  	e.preventDefault();
+	const inputText = document.getElementById('crname').value;
+	
+	if(inputText.length < 5) {
+		alert('Room name must be at least 5 characters');
+	} else {
+		const newRoomName = this.state.newRoomName;
+		this.roomsRef.push({ name: newRoomName });
+		this.setState({ newRoomName: '' });
 	}
+  }
 
-	handleNameChange(e){
-		this.setState({ newRoomName: e.target.value });
-	}
-	render(){
-		return(
-			<div className="roomlist">
-				<h2>Rooms</h2>
-				<ul>{this.state.rooms.map( (room, index) => {
-					return(<li className="room" key={index}>{room.name}</li>)
-					})}
-				</ul>
-				<form onSubmit={ (e) => this.createRoom(e) }>
-					Start a new chat room: 
-					<input id="crname" 
-							type="text" 
-							placeholder="e.g. My Room"
-							value={ this.state.newRoomName }
-							onChange={ (e) => this.handleNameChange(e) } />
-					<input type="submit" value="Create New Room" />
-				</form>
-			</div>
-		);
-	}
+  handleNameChange(e) {
+	this.setState({ newRoomName: e.target.value });
+  }
+
+  selectRoom(room) {
+  	this.props.activeRoom(room);
+  }
+
+  render() {
+  	return(
+  		<div className='roomlist'>
+  		  <ul>{this.state.rooms.map( (room, index) => {
+  		  		return(
+  		  			<div className='room' key={index} onClick={ (e) => this.selectRoom(room, e) }>{room.name}</div>
+  		  	  	);
+			  })}
+		  </ul>
+		  <form onSubmit={ (e) => this.createRoom(e) }> Start a new chat room:
+		  	<input id='crname' type='text' placeholder='e.g. My Room' value={this.state.newRoomName} onChange={ (e) => this.handleNameChange(e) } />
+		  	<input type='submit' value='Create Room' />
+		  </form>
+		</div>
+	);
+  }
 }
 
 export default RoomList;
